df95d8e31c25a98337e33169185446fa103ae0ff
A-Tr <alvarotrancon@gmail.com>
2018-05-07 10:25:01 +0200

    model list

--- server/api/list/list.model.js
+++ server/api/list/list.model.js
@@ -1,10 +1,31 @@
-'use strict';
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const listSchema = new mongoose.Schema({
-  // Lesson 2: Implement the List Model
-});
+const listSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'title is required']
+    },
+    position: {
+      type: Number,
+      default: 0
+    },
+    cards: [
+      {
+        card: {
+          type: Schema.Types.ObjectId,
+          ref: 'Card'
+        }
+      }
+    ]
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  }
+);
 
 module.exports = mongoose.model('List', listSchema);
